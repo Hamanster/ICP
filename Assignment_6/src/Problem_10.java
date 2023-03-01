b76e484a6ca0b7156991dd532b11d103703ad4ff
Aman Agarwal <106427948+Hamsterbhaiya@users.noreply.github.com>
2023-03-01 23:48:24 +0530

    ITER SEMESTER 1 ICP ASSIGNMENTS

--- Assignment_6/src/Problem_10.java
+++ Assignment_6/src/Problem_10.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 public class Problem_10 {
     public static void main(String[] args) {
-        Scanner x = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         
                 System.out.println("Enter the base and height of the triangle:");
-                double base = x.nextDouble();
-                double height = x.nextDouble();
+                double base = sc.nextDouble();
+                double height = sc.nextDouble();
                 System.out.println("Area of triangle: " + ar(base, height));
 
                 System.out.println("Enter the side length of the square:");
-                double side = x.nextDouble();
+                double side = sc.nextDouble();
                 System.out.println("Area of square: " + ar(side));
 
                 System.out.println("Enter the radius of the circle:");
-                float radius = x.nextFloat();
+                float radius = sc.nextFloat();
                 System.out.println("Area of circle: " + ar(radius));
 
                 System.out.println("Enter the length and width of the rectangle:");
-                float length = x.nextFloat();
-                float width = x.nextFloat();
+                float length = sc.nextFloat();
+                float width = sc.nextFloat();
                 System.out.println("Area of rectangle: " + ar(length, width));
 
         }
