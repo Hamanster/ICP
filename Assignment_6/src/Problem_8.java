7a3fb84acf563724620259f77ed9b47592d5c5d5
Aman Agarwal <106427948+Hamsterbhaiya@users.noreply.github.com>
2023-03-01 23:02:55 +0530

    ITER SEMESTER 1 ICP ASSIGNMENTS

--- Assignment_6/src/Problem_8.java
+++ Assignment_6/src/Problem_8.java
@@ -1,8 +1,21 @@
 import java.util.Scanner;
 public class Problem_8 {
 	public static boolean pali(String input) {
-	    String cleaned = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-	    return cleaned.equals(new StringBuilder(cleaned).reverse().toString());
+		String p="";
+		input = input.toLowerCase();
+		for(int i =input.length()-1;i>=0;i--) {
+			p = p + input.charAt(i);
+		}
+//		return p.equals(input); we can use .equals() which is a String function,
+//		but it is not taught tu us, so I will use a different method.
+		boolean flag = true;
+		for(int i = 0;i<p.length();i++){
+			if(p.charAt(i)!=input.charAt(i)){
+				flag =false;
+				break;
+			}
+		}
+		return flag;
 	}
 	public static void main(String[] args) {
 	Scanner x = new Scanner(System.in);
