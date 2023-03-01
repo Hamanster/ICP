9152c29ca0ca43a35dc441bc705bc71d10517f5e
Aman Agarwal <106427948+Hamsterbhaiya@users.noreply.github.com>
2023-03-01 23:09:27 +0530

    ITER SEMESTER 1 ICP ASSIGNMENTS

--- Assignment_6/src/Problem_7.java
+++ Assignment_6/src/Problem_7.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 public class Problem_7 {
 public static int count(String str) {
     int count = 0;
+    str=str.toLowerCase();
     for (int i = 0; i < str.length(); i++) {
         char c = str.charAt(i);
-        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
-                || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
             count++;
         }
     }
     return count;
 }
 public static void main(String[] args) {
-	Scanner x = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
 	System.out.print("Enter a string: ");
-    String str = x.nextLine();
+    String str = sc.nextLine();
     int vowelCount = count(str);
     System.out.println("The number of vowels in \"" + str + "\" is " + vowelCount);
 }
